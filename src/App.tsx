5a6c27dd3705f278f98951d2992eba516f271cd3
Abhishek kumar <sahaabhishek443@gmail.com>
2024-12-05 20:58:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashBoard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashBoard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
---
+++ src/components/city-search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CitySearch = () => {
+  return <div>CitySearch</div>;
+};
+
+export default CitySearch;
