fa442694297f0594841744933c56ba245719bc62
Abhishek kumar <sahaabhishek443@gmail.com>
2024-12-08 09:09:13 +0530

    resolve error

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -4,7 +4,9 @@ import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
 
 import { cn } from "../../lib/utils";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+// import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog } from "./dialog";
+import { DialogContent } from "./dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
